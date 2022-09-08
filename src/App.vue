ce2d111fa1b521ebcf62827fb2af38c306772ab1
Rakesh Roshan M N <rakeshroshan@gmail.com>
2022-09-08 22:04:16 +0530

    sm change

--- src/App.vue
+++ src/App.vue
@@ -40,20 +40,6 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>HICTOUS</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
